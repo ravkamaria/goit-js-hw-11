dd70140d7a368f767fa506f14a5526bba1dfc7b6
ravkamaria <hevkomaria1@gmail.com>
2023-08-31 15:04:51 +0300

    added axios and createmarkup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import axios from "axios";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const baseURL = "https://pixabay.com/api/";
+// axios.defaults.headers.common['key'] = '39131974-7ca5284b81c975fe0ad28b146';
 
 const elements = {
-    form: document.querySelector("#search-form",)
+    form: document.querySelector("#search-form"),
+    container: document.querySelector(".gallery"),
+    btnLoadMore: document.querySelector(".load-more")
 }
 
 elements.form.addEventListener("submit", handlerSearch);
@@ -9,7 +15,52 @@ elements.form.addEventListener("submit", handlerSearch);
 function handlerSearch(evt){
     evt.preventDefault();
     const input = elements.form.elements.searchQuery.value;
-    console.log(input);
+    searchGet(input);
+}
 
+function searchGet(word){
+    elements.container.innerHTML = "";
+    const params = new URLSearchParams({
+        key: '39131974-7ca5284b81c975fe0ad28b146',
+        q: word,
+        image_type: "photo",
+        orientation: "horizontal",
+        safesearch: true
+});
+    axios.get(`${baseURL}?${params}`)
+    .then(resp => {
+        const arrPhoto = resp.data.hits;
+        if(arrPhoto !== []){
+            createMarkUp(arrPhoto);
+        }else{
+            Notify.failure("Sorry, there are no images matching your search query. Please try again."); //Не відображається
+            console.log("problem")
+        }
+    })
+    .catch(err => {
+        console.log(err)
+    });
+    
+}
+
+function createMarkUp(arr){
+    const markUp = arr.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => `<div class="photo-card">
+    <img src="${largeImageURL}" alt="${tags}" loading="lazy" />
+    <div class="info">
+      <p class="info-item">
+        <b>Likes: ${likes}</b>
+      </p>
+      <p class="info-item">
+        <b>Views: ${views}</b>
+      </p>
+      <p class="info-item">
+        <b>Comments: ${comments}</b>
+      </p>
+      <p class="info-item">
+        <b>Downloads: ${downloads}</b>
+      </p>
+    </div>
+  </div>`).join("");
+   elements.container.insertAdjacentHTML("beforeend", markUp);
 
 }
