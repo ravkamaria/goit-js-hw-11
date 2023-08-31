eceb6b5dca5bd176f7e3e4ec9a028e3a08207987
ravkamaria <131477170+ravkamaria@users.noreply.github.com>
2023-08-31 12:05:42 +0000

    Deploying to gh-pages from @ ravkamaria/goit-js-hw-11@dd70140d7a368f767fa506f14a5526bba1dfc7b6 🚀

--- index.6a7045bd.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={form:document.querySelector("#search-form")};e.form.addEventListener("submit",(function(r){r.preventDefault();var o=e.form.elements.searchQuery.value;console.log(o)}))}();
-//# sourceMappingURL=index.6a7045bd.js.map
--- index.9b3a9e98.js
+++
@@ -1,2 +0,0 @@
-const e={form:document.querySelector("#search-form")};e.form.addEventListener("submit",(function(o){o.preventDefault();const r=e.form.elements.searchQuery.value;console.log(r)}));
-//# sourceMappingURL=index.9b3a9e98.js.map
