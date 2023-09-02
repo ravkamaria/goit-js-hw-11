06c66a699788d455feadf9afb42bfcee66d11580
ravkamaria <hevkomaria1@gmail.com>
2023-09-03 01:52:00 +0300

    added SimpleLightBox

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import axios from "axios";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const baseURL = "https://pixabay.com/api/";
 // axios.defaults.headers.common['key'] = '39131974-7ca5284b81c975fe0ad28b146';
 
+console.log("hello")
 const elements = {
     form: document.querySelector("#search-form"),
     container: document.querySelector(".gallery"),
     btnLoadMore: document.querySelector(".load-more")
 }
+// let gallery = new SimpleLightbox(elements.container);
+// let lightbox = new SimpleLightbox('.photo-card', { /* options */ });
+
+
 
 elements.form.addEventListener("submit", handlerSearch);
 
@@ -18,8 +25,11 @@ function handlerSearch(evt){
     searchGet(input);
 }
 
+let box = new SimpleLightbox('.photo-card a');
+
 function searchGet(word){
-    elements.container.innerHTML = "";
+    elements.container.innerHTML = "";   
+
     const params = new URLSearchParams({
         key: '39131974-7ca5284b81c975fe0ad28b146',
         q: word,
@@ -30,7 +40,8 @@ function searchGet(word){
     axios.get(`${baseURL}?${params}`)
     .then(resp => {
         const arrPhoto = resp.data.hits;
-        if(arrPhoto !== []){
+        console.log(arrPhoto);
+        if(arrPhoto.length){
             createMarkUp(arrPhoto);
         }else{
             Notify.failure("Sorry, there are no images matching your search query. Please try again."); //Не відображається
@@ -40,11 +51,12 @@ function searchGet(word){
     .catch(err => {
         console.log(err)
     });
-    
 }
 
+
+
 function createMarkUp(arr){
-    const markUp = arr.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => `<div class="photo-card">
+    const markUp = arr.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => `<div class="photo-card"><a href="${webformatURL}">
     <img src="${largeImageURL}" alt="${tags}" loading="lazy" />
     <div class="info">
       <p class="info-item">
@@ -60,7 +72,7 @@ function createMarkUp(arr){
         <b>Downloads: ${downloads}</b>
       </p>
     </div>
-  </div>`).join("");
+  </a></div>`).join("");
    elements.container.insertAdjacentHTML("beforeend", markUp);
-
+  box.refresh();
 }
